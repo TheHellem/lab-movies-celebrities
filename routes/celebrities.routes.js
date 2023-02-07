74bb20be7159db941d3d6e88741d47a0aa63b1c2
HenrikH <henrik.hellem@live.com>
2023-02-07 15:57:56 +0100

    Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -26,7 +26,14 @@ router.post('/celebrities/create', (req, res, next) =>{
     })
 })
 
-
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+    .then( celebritiesFromDB => {
+        console.log(celebritiesFromDB)
+        res.render('celebrities/celebrities', {celebrities : celebritiesFromDB})
+    })
+    .catch(err => next(err))
+})
 
 
 module.exports = router;
