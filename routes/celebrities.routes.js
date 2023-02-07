ec75cb3e7b7735ef482603392d9644c5934c198d
HenrikH <henrik.hellem@live.com>
2023-02-07 16:55:07 +0100

    Iteration 6+7

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,37 +3,34 @@ const { application } = require("express");
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+const Celebrity = require("../models/Celebrity.model");
 
-const Celebrity = require('../models/Celebrity.model')
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
 
-router.get('/celebrities/create', (req, res, next) =>{
-    res.render('celebrities/new-celebrity')
-})
+router.post("/celebrities/create", (req, res, next) => {
+  console.log(req.body);
+  const { name, occupation, catchPhrase } = req.body;
 
-
-router.post('/celebrities/create', (req, res, next) =>{
-    console.log(req.body)
-	const { name, occupation, catchPhrase} = req.body
-
-    Celebrity.create({name, occupation, catchPhrase})
-    .then(createdCelebrity => {
-        console.log(createdCelebrity)
-        res.redirect(`celebrities/${createdCelebrity._id}`)
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((createdCelebrity) => {
+      console.log(createdCelebrity);
+      res.redirect(`celebrities/${createdCelebrity._id}`);
     })
-    .catch( err =>{
-        res.redirect('celebrities/new-celebrity')
-        next(err)
-    })
-})
+    .catch((err) => {
+      res.redirect("celebrities/new-celebrity");
+      next(err);
+    });
+});
 
-router.get('/celebrities', (req, res, next) => {
-    Celebrity.find()
-    .then( celebritiesFromDB => {
-        console.log(celebritiesFromDB)
-        res.render('celebrities/celebrities', {celebrities : celebritiesFromDB})
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDB) => {
+      console.log(celebritiesFromDB);
+      res.render("celebrities/celebrities", { celebrities: celebritiesFromDB });
     })
-    .catch(err => next(err))
-})
-
+    .catch((err) => next(err));
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,4 +3,37 @@ const router = require("express").Router();
 
 // all your routes here
 
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find().then((celebritiesFromDB) => {
+    res.render("movies/new-movie", { celebrities: celebritiesFromDB });
+  });
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.create({ title, genre, plot, cast })
+    .then((createdMovie) => {
+      console.log(createdMovie);
+      res.redirect(`movies`);
+    })
+    .catch((err) => {
+      res.redirect("movies/new-movie");
+      next(err);
+    });
+});
+
+  
+  router.get("/movies", (req, res, next) => {
+    Movie.find()
+      .then((moviesFromDB) => {
+        console.log(moviesFromDB);
+        res.render("movies/movies", { celebrities: moviesFromDB });
+      })
+      .catch((err) => next(err));
+  });
+
 module.exports = router;
