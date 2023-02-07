988e54092aa9bf11ef66d7210ec5bd1632e2cfb5
HenrikH <henrik.hellem@live.com>
2023-02-07 15:44:59 +0100

    Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -6,12 +6,26 @@ const router = require("express").Router();
 
 const Celebrity = require('../models/Celebrity.model')
 
-// all your routes here
-
-
 router.get('/celebrities/create', (req, res, next) =>{
     res.render('celebrities/new-celebrity')
 })
+
+
+router.post('/celebrities/create', (req, res, next) =>{
+    console.log(req.body)
+	const { name, occupation, catchPhrase} = req.body
+
+    Celebrity.create({name, occupation, catchPhrase})
+    .then(createdCelebrity => {
+        console.log(createdCelebrity)
+        res.redirect(`celebrities/${createdCelebrity._id}`)
+    })
+    .catch( err =>{
+        res.redirect('celebrities/new-celebrity')
+        next(err)
+    })
+})
+
 
 
 
